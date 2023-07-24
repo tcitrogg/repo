3782e6a2aeaf2b2d151bfcbec6ea18e91019fa2f
bnierimi <shemiradiance@gmail.com>
2023-07-24 14:45:40 +0100

    t/bnierimi: (+) Updated `softrays/web_dev/07242023.py`

--- softrays/web_dev/07242023.py
+++ softrays/web_dev/07242023.py
@@ -10,24 +10,30 @@
 # ~ activity: Guess number game
 from random import randint
 from pyfiglet import Figlet
+# pip install pyfiglet==0.7
+from colorama import init, Fore
+# pip install colorama
 
 f = Figlet(font="slant")
+init(autoreset=True)
 
-print(f.renderText("~~ GNG ~~"))
+print(Fore.RED + f.renderText("~~ GNG ~~"))
 print("~~ Guess Number Game ~~")
 
 correct_number = randint(0, 9)
-chances = 5
+chances = 3
 
 for i in range(chances):
+    no_chances = Fore.BLUE + f"{chances - i}" + Fore.RESET
     try:
-        answer = int(input(f"[{chances - i}] Guess a number: "))
+        answer = int(input(f"[{no_chances}] Guess a number: " + Fore.CYAN))
+        print(Fore.RESET, end="")
         if answer == correct_number:
-            print("(+) Yeah! correct! :)")
+            print(Fore.GREEN + "(+) Yeah! correct! :)" + Fore.RESET)
             break
     except ValueError:
-        print(f"  (x) Error : enter number only")
+        print(Fore.RED + f"  (x) Error : enter number only" + Fore.RESET)
     except Exception as error_message:
-        print(f"  (x) Error : {error_message}")
+        print(Fore.RED + f"  (x) Error : {error_message}" + Fore.RESET)
 else:
     print("(x) Nah! all wrong :(")
